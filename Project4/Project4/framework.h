262b486ecbe24c298f0bdf128678cfeafe096711
evespimrose <71066188+evespimrose@users.noreply.github.com>
2021-11-09 16:03:38 +0900

    struct Player_data, 소켓 생성 함수 커밋

--- Project4/Project4/framework.h
+++ Project4/Project4/framework.h
@@ -20,3 +20,58 @@
 #include <stdio.h>
 #include <time.h>
 #include <chrono>
+
+// 클라이언트 통신 관련 참조 헤더
+#define _CRT_SECURE_NO_WARNINGS
+#pragma comment(lib, "ws2_32")
+#include <winsock2.h>
+
+struct Player_data
+{
+    glm::vec3 PosVec;
+    float speed;
+    string ID;
+    vector<Bullet> BulletList;
+    bool KeyDownlist[3];
+};
+
+
+// 소켓 함수 오류 출력 후 종료
+void err_quit(char* msg)
+{
+    LPVOID lpMsgBuf;
+    FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, WSAGetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&lpMsgBuf, 0, NULL);
+    MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+    LocalFree(lpMsgBuf);
+    exit(1);
+}
+
+// 소켓 함수 오류 출력
+void err_display(char* msg)
+{
+    LPVOID lpMsgBuf;
+    FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, WSAGetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&lpMsgBuf, 0, NULL);
+    printf("[%s] %s", msg, (char*)lpMsgBuf);
+    LocalFree(lpMsgBuf);
+}
+
+int Clientsock(int argc, char** argv)
+{
+    int retval;
+
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+        return 1;
+
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    SOCKADDR_IN serveraddr;
+    ZeroMemory(&serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = inet_addr(argv[2]);
+    serveraddr.sin_port = htons(atoi(argv[3]));
+
+    retval = connect(sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
+
+
+}
