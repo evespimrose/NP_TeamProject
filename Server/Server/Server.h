2e8d9b9ff131420463e1f71dc508c39a9cd537b3
kimjunho403 <kimjunho403@gmail.com>
2021-12-12 01:13:46 +0900

    코드 정리

--- Server/Server/Server.h
+++ Server/Server/Server.h
@@ -7,12 +7,6 @@ struct Message {
     char dir;
     bool isPushed;
 };
-
-// structs for send
-//typedef struct Data
-//{
-//    Player_data PlayerData;
-//}Data;
 using namespace std;
 
 typedef struct Col_Player_data //계산용 플레이어 데이터
@@ -25,8 +19,6 @@ typedef struct Col_Player_data //계산용 플레이어 데이터
     float rad = 0.0f;
 
     float Speed = 0.0f;
-    glm::mat4 TR = RotMat * PosMat * SclMat;
-
 }Col_Player_data;
 
 typedef struct Col_Bullet_data//계산용 총알 데이터
@@ -34,7 +26,7 @@ typedef struct Col_Bullet_data//계산용 총알 데이터
     float rotate = 0.0f;
     glm::mat4 PosMat = glm::mat4(1.0f);
     float Speed = 0.0f;
-    glm::vec3 PosVec = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 PosVec = glm::vec3(0.0f, 0.0f, -50.0f);
     glm::mat4 RotMat = glm::mat4(1.0f); 
 }Col_Bullet_data;
 
@@ -48,19 +40,15 @@ typedef struct Col_Cube_data {
 
 typedef struct Player_data//최종 보낼 플레이어 데이터
 {
-    //std::vector<int> BulletList;
     glm::vec3 Posvec = glm::vec3(0.0f, 0.0f, 0.0f);
     glm::mat4 PosMat= glm::mat4(1.0f);
     glm::mat4 SclMat= glm::mat4(1.0f);
     glm::mat4 RotMat= glm::mat4(1.0f);
-   // glm::mat4 TR = glm::mat4(1.0f);
     float rad = 0.0f;
     
 }Player_data;
 
-
-
-typedef struct Bullet_data//최종 보낼 플레이어 데이터
+typedef struct Bullet_data//최종 보낼 총알 데이터
 {
     std::vector<int> BulletList;
     glm::vec3 Posvec = glm::vec3(0.0f, 0.0f, 0.0f);
@@ -75,17 +63,6 @@ typedef struct Bullet_data//최종 보낼 플레이어 데이터
 
 }Bullet_data;
 
-typedef struct Game_data//최종 보낼 플레이어 데이터
-{
-    Player_data player_data[3];
-  //  Bullet_data bullet_data[100];
-}Game_data;
-
-
-LARGE_INTEGER tSecond;
-LARGE_INTEGER tTime;
-float acc= 0.0001f;
-float fDeltaTime=0;
 
 
 typedef struct ready_info
@@ -99,8 +76,6 @@ typedef struct ready_info
 
 bool is_connected[3] = { false, };
 
-//ready_info* ri; //보냄
-
 ready_info ri[3];
 
 SOCKET Client_sock[3];
@@ -109,3 +84,10 @@ std::queue <Message> glo_MsgQueue;
 
 int count_s = -1;
 int Bullet_num = 0;//발사된 총알 개수
+
+//타이머 관련
+LARGE_INTEGER tSecond;
+LARGE_INTEGER tTime;
+float acc = 0.0001f;
+float fDeltaTime = 0;
+
