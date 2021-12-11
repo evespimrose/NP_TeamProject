0496bd8388cfd9d70f5519ef41741dbd3d2d3ab9
evespimrose <71066188+evespimrose@users.noreply.github.com>
2021-12-11 23:44:58 +0900

    충돌체크 프로토타입.

--- Server/Server/소스.cpp
+++ Server/Server/소스.cpp
@@ -525,13 +525,36 @@ void Calcutlaion_clients() {
 		for (int i = 0; i < count_s + 1; ++i) {
 			for (int j = 0; j < MAX_CUBE; ++j)
 			{
-				if (col_player_data[i].Posvec.z + 0.5f > ccd[j].PosZ &&
-					col_player_data[i].Posvec.z - 0.5f < ccd[j].PosZ &&
-					col_player_data[i].rad + 10 > ccd[j].rad &&
-					col_player_data[i].rad - 10 < ccd[j].rad
+				// 각도 처리
+				float plus_rad = col_player_data[i].rad + 10;
+				if (plus_rad > 360)
+				{
+					plus_rad = plus_rad - 360;
+				}
+				float minus_rad = col_player_data[i].rad - 10;
+				if (minus_rad < 0)
+				{
+					minus_rad = minus_rad + 360;
+				}
+				// 비교
+				if (col_player_data[i].Posvec.z + 0.1f > ccd[j].PosZ &&
+					col_player_data[i].Posvec.z - 0.1f < ccd[j].PosZ &&
+					plus_rad > ccd[j].rad &&
+					minus_rad < ccd[j].rad
 					)
 				{
+					cout << "플레이어" << i << ", rad " << col_player_data[i].rad << ", 큐브" << j << ", rad" << ccd[j].rad << endl;
 					// 충돌처리 추가
+					col_player_data[i].Speed /= 2;
+
+					Col_Cube_data c;
+					c.life = rand() % 3;
+					c.PosZ = fpz + 100.0f + rand() % 100;
+					glm::vec3 cpos = glm::vec3(0.0f, -3.5f, c.PosZ);
+					c.rad = rand() % 360;
+					c.PosMat = glm::translate(c.PosMat, cpos);
+					c.RotMat = glm::rotate(c.RotMat, glm::radians(c.rad), glm::vec3(0.0f, 0.0f, 1.0f));
+					ccd[j] = c;
 				}
 			}
 		}
